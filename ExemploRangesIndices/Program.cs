a29ddb0e97f0b4352c2d9d0e329d897b241a2d2a
Renato Groffe <renato.groffe@yahoo.com.br>
2020-01-05 22:01:23 -0300

    .NET Core 3.1 + Console Application + C #8.0 + Ranges + Indices
    .NET Core 3.1 + Console Application + C #8.0 + Ranges + Indices

---
+++ ExemploRangesIndices/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ExemploRangesIndices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tecnologias =
+                new string[] { ".NET Core", "Angular", "ASP.NET Core", "Azure", "C#", "Docker" };
+
+            ImprimirElementos(tecnologias[2..5],
+                "Retornando elementos dos índices 2 a 4");
+
+            Range range = 2..5;
+            ImprimirElementos(tecnologias[range],
+                "Retornando elementos dos índices 2 a 4 via struct Range");
+
+            ImprimirElementos(tecnologias[0..5],
+                "Retornando elementos dos índices 0 a 4");
+            ImprimirElementos(tecnologias[..5],
+                "Retornando até o elemento de posição 4 (desde a posição 0)");
+
+            ImprimirElementos(tecnologias[1..^2],
+                "Retornando elementos da posição 1 até o antepenúltimo");
+            ImprimirElementos(tecnologias[1..^1],
+                "Retornando elementos da posição 1 até o penúltimo");
+            ImprimirElementos(tecnologias[..^1],
+                "Retornando até o penúltimo elemento");
+        }
+
+        private static void ImprimirElementos(string[] selecaoTecnologias,
+            string mensagem)
+        {
+            Console.Write($"{mensagem}: [");
+            foreach (string tecnologia in selecaoTecnologias)
+            {
+                Console.Write($"  *{tecnologia}*");
+            }
+            Console.Write("  ]" + Environment.NewLine);
+        }
+    }
+}
